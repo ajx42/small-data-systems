4e66a79e7d2b1ec94ce7b283d946e7e9e2c342a2
Aditya Jain <adityajain.aj10@gmail.com>
2024-04-28 23:52:20 -0500

    update kernel mix

--- project/plot/plot_kernel_mix.py
+++ project/plot/plot_kernel_mix.py
@@ -1,5 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure(figsize=(16, 10))
 
 with open('../data/nccl_kernel_sum/communication_kernel_sum.txt', 'r') as file:
     csv_reader = csv.reader(file)
@@ -17,13 +20,15 @@ for row in rows:
     x = tm * 100.0 / pct
     data['(TP={},PP={})'.format(row['TP'], row['PP'])] = (int(tm), int(x))
 
+colors = plt.cm.Reds(np.linspace(0.2, 1, 3))
+
 x_lab = data.keys()
 main_bars = [float(data[x][1]-data[x][0])/10**9 for x in x_lab]
 sub_bars = [float(data[x][0])/10**9 for x in data]
 
-plt.bar(x_lab, main_bars, width=0.35, label='Other Kernels')
-plt.bar(x_lab, sub_bars, width=0.35, label='Communication Kernels', bottom=main_bars)
-
+plt.bar(x_lab, main_bars, width=0.35, label='Other Kernels', color=colors[0])
+plt.bar(x_lab, sub_bars, width=0.35, label='Communication Kernels', bottom=main_bars, color=colors[1])
+plt.xticks(rotation=45)
 plt.ylabel('Total Kernel Running Time')
 
 plt.xlabel('Configuration')
